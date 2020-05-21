191f683a1c3975c1acbb7a1cade4c74eef24728f
Zyryanova Maria <mariazyranova72@gmail.com>
2020-05-21 17:53:57 +0500

    + Создана база данных

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import os
 from flask import Flask, render_template
 import logging
 
-
+from data import db_session
+from data.battle import Battle
 logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
+db_session.global_init("db/battle.sqlite")
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('main.html', title='Главная страница')
+    session = db_session.create_session()
+    battles = session.query(Battle).all()
+    return render_template('main.html', title='Главная страница', battles=battles)
 
 
 if __name__ == '__main__':
