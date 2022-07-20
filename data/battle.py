e0465042db8bda9e565256bf021bea4b84d59df1
MariaZyryanova72 <mariyazz@inbox.ru>
2022-07-20 17:35:35 +0500

    update db, requirements.txt

--- data/battle.py
+++ data/battle.py
@@ -1,41 +1,40 @@
-import sqlalchemy
-from .db_session import SqlAlchemyBase
+from main import db
 
 
-class Battle(SqlAlchemyBase):
+class Battle(db.Model):
     __tablename__ = 'battle'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
 
-    preview_img = sqlalchemy.Column(sqlalchemy.String)
+    preview_img = db.Column(db.String)
 
-    text = sqlalchemy.Column(sqlalchemy.String)
-    text_img_1 = sqlalchemy.Column(sqlalchemy.String)
-    text_img_2 = sqlalchemy.Column(sqlalchemy.String)
-    text_img_3 = sqlalchemy.Column(sqlalchemy.String)
-    text_img_4 = sqlalchemy.Column(sqlalchemy.String)
-    text_img_5 = sqlalchemy.Column(sqlalchemy.String)
-    text_img_6 = sqlalchemy.Column(sqlalchemy.String)
-    text_img_7 = sqlalchemy.Column(sqlalchemy.String)
+    text = db.Column(db.String)
+    text_img_1 = db.Column(db.String)
+    text_img_2 = db.Column(db.String)
+    text_img_3 = db.Column(db.String)
+    text_img_4 = db.Column(db.String)
+    text_img_5 = db.Column(db.String)
+    text_img_6 = db.Column(db.String)
+    text_img_7 = db.Column(db.String)
 
-    articles_1 = sqlalchemy.Column(sqlalchemy.String)
-    articles_img_1 = sqlalchemy.Column(sqlalchemy.String)
+    articles_1 = db.Column(db.String)
+    articles_img_1 = db.Column(db.String)
 
-    articles_2 = sqlalchemy.Column(sqlalchemy.String)
-    articles_img_2 = sqlalchemy.Column(sqlalchemy.String)
+    articles_2 = db.Column(db.String)
+    articles_img_2 = db.Column(db.String)
 
-    articles_3 = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    articles_img_3 = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    articles_3 = db.Column(db.String, nullable=True)
+    articles_img_3 = db.Column(db.String, nullable=True)
 
-    articles_4 = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    articles_img_4 = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    articles_4 = db.Column(db.String, nullable=True)
+    articles_img_4 = db.Column(db.String, nullable=True)
 
-    articles_5 = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    articles_img_5 = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    articles_5 = db.Column(db.String, nullable=True)
+    articles_img_5 = db.Column(db.String, nullable=True)
 
-    video_1 = sqlalchemy.Column(sqlalchemy.String)
+    video_1 = db.Column(db.String)
 
-    video_2 = sqlalchemy.Column(sqlalchemy.String)
+    video_2 = db.Column(db.String)
 
-    video_3 = sqlalchemy.Column(sqlalchemy.String)
+    video_3 = db.Column(db.String)
--- main.py
+++ main.py
@@ -5,34 +5,38 @@ import random
 from flask import Flask, render_template
 import logging
 
-from data import db_session
-from data.battle import Battle
 logging.basicConfig(level=logging.INFO)
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
-db_session.global_init("db/battle.sqlite")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/battle.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['FLASK_ADMIN_SWATCH'] = 'cosmo'
+app.config['BABEL_DEFAULT_LOCATE'] = 'ru'
+
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    session = db_session.create_session()
-    battles = session.query(Battle).all()
+    from data.battle import Battle
+    battles = Battle.query.all()
     battles = random.sample(battles, len(battles))[:6]
     return render_template('main.html', title='Главная страница', battles=battles)
 
 
 @app.route('/battle/<int:battle_id>', methods=['GET'])
 def battle(battle_id):
-    session = db_session.create_session()
-    battle = session.query(Battle).filter(Battle.id == battle_id).first()
+    from data.battle import Battle
+    battle = Battle.query.filter(Battle.id == battle_id).first()
     return render_template('battle.html', title=battle.title, battle=battle)
 
 
 @app.route('/events', methods=['GET'])
 def events():
-    session = db_session.create_session()
-    events = session.query(Battle).all()
+    from data.battle import Battle
+    events = Battle.query.all()
     return render_template('events.html', title="События", events=events)
 
 
